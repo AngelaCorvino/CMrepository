e07d28f2a11033d55efa60998f5e9f4a6187bb0c
a.corvino@hzdr.de <gelato20>
2021-10-03 17:19:59 +0200

    The program calculates the frequencies. Isthogram to be done.

--- 1.py
+++ 1.py
@@ -5,11 +5,36 @@ import time
 import string
 import argparse
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-log",
+    "--log",
+    default="warning",
+    help=(
+        "Provide logging level. "
+        "Example --log debug', default='warning'"),
+    )
 
-
-
+options = parser.parse_args()
+levels = {
+    'critical': logging.CRITICAL,
+    'error': logging.ERROR,
+    'warn': logging.WARNING,
+    'warning': logging.WARNING,
+    'info': logging.INFO,
+    'debug': logging.DEBUG
+}
+level = levels.get(options.log.lower())
+if level is None:
+    raise ValueError(
+        f"log level given: {options.log}"
+        f" -- must be one of: {' | '.join(levels.keys())}")
+logging.basicConfig(level=level)
+logger = logging.getLogger(__name__)
+"""
 
 
 def process(file_path):
@@ -18,36 +43,35 @@ def process(file_path):
     logging.info("Reading input file %s ..",file_path)
 
 
-    """
+
     with open(file_path) as input_file:   #il with è un context manager stiamo dicendo inputfile=open(file_path) solo ora quando usciamo da questo blocco di codice il file viene aiutomaticamente chiuso
         text=input_file.read() #mi restituisce una stringa
     num_chars=len(text)
     logging.info("Done,%d characters found.",num_chars)
     #char_dict={chr(x):0 for x in range(ord('a'),ord('z')+1)}     #dizionario vuoto
-    char_dict={ch:0 for ch in string.ascii_lowercase}
-    """
+    #The ord() method in Python converts a character into its Unicode code value
+    char_dict={ch:0 for ch in string.ascii_lowercase}  #creo un dizionario in cui la chiave è la letta del dizionario e inizialemnte ad ogni lettera è associato 0
+    """ In Python, string ascii_lowercase will give the lowercase letters ‘abcdefghijklmnopqrstuvwxyz """
+    for ch in text:
+        #ch=ch.lower()
+        if ch in char_dict:
+            char_dict[ch]+=1
+        #try :
+        #    char_dict[ch.lower()]+=1  #trasformiamo le maiuscole in minuscole
+        #except KeyError:
+        #    pass
+
+    num_letters=sum(char_dict.values())
+    for ch, num in char_dict.items():
+        print(f"{ch}->{num/num_letters:.3%}")
+
     elapsed_time=time.time()-start_time
     logging.info("Done in %.3f seconds.",elapsed_time)
+
 
 if __name__ =="__main__" :  #
     parser = argparse.ArgumentParser()
     parser.add_argument('infile',type=str, help="Path to the input file")
     args=parser.parse_args()
+
     process(args.infile)
-
-"""
---- Goal
-Write a Python program that prints the relative frequence of each letter
-of the alphabet (without distinguishing between lower and upper case) in the
-book.
-
---- Specifications
-- the program should have a --help option summarizing the usage
-- the program should accept the path to the input file from the command line
-- the program should print out the total elapsed time
-- the program should have an option to display a histogram of the frequences
-- [optional] the program should have an option to skip the parts of the text
-  that do not pertain to the book (e.g., preamble and license)
-- [optional] the program should have an option to print out the basic book
-  stats (e.g., number of characters, number of words, number of lines, etc.)
-"""
